53c6bac3b5bc0229f582a3557f67013cdd0cf6cb
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-10-20 16:12:02 -0500

    v0.24.0 Se agrega  información de nombre, domicilio y precio en carta casa hogar Pasitea

--- templates/lista_casas/hogar-pasitea/_carta_casa_detalles.php
+++ templates/lista_casas/hogar-pasitea/_carta_casa_detalles.php
@@ -9,16 +9,14 @@ if(count($a)>2) {
     $costo=$b[0]."'".$b[1];
 }
 ?>
-<div class="property-card-box card-box card-block">
-    <h3 class="property-card-title"><a href="<?php echo $generales->url_base; ?>hogares/hogar-pasitea.php">Hogar Pasítea</a></h3>
-    <div class="property-card-descr">
-        Hacienda del Agave #167 Condominio 6, Hacienda Real. Tonala, Jalisco
-    </div>
-    <div class="property-preview-footer  clearfix">
-        <div class="property-preview-f-left text-color-primary">
-            <span class="property-card-value">
+<div class="property-card-box card-box card-block estilo_carta_casa_texto">
+    <h3 class="titulo_carta_casa"><a class="titulo_carta_casa" href="<?php echo $generales->url_base; ?>hogares/hogar-pasitea.php">Hogar Pasítea</a></h3>
+    <div class="property-card-descr texto_carta_casa">
+        <p class="texto_carta_casa">Hacienda del Agave #167 Condominio 6,</p>
+        <p class="texto_carta_casa">Hacienda Real. Tonala, Jalisco</p>
+        <span class="property-card-value" id="texto_carta_casa">
                 <i class="fa fa-dollar"><?php echo $costo ?></i>
-            </span>
-        </div>
+        </span>
     </div>
+
 </div>
