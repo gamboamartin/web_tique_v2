f0ac7422b9a76bb8eab3359775ea9f46af281329
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-10-20 17:10:52 -0500

    v0.28.0 Se agrega  información de nombre, domicilio y precio en carta casa hogar Beatriz

--- templates/lista_casas/hogar-beatriz/_carta_casa_detalles.php
+++ templates/lista_casas/hogar-beatriz/_carta_casa_detalles.php
@@ -9,14 +9,14 @@ if(count($a)>2) {
     $costo=$b[0]."'".$b[1];
 }
 ?>
-<div class="property-card-box card-box card-block">
-    <h3 class="property-card-title"><a href="<?php echo $generales->url_base; ?>hogares/hogar-beatriz.php">Hogar Beatriz</a></h3>
-    <div class="property-card-descr"> Dirección Lázaro Pavia 3436, Beatriz Hernández, 44760</div> <br>
-    <div class="property-preview-footer  clearfix">
-        <div class="property-preview-f-left text-color-primary">
-            <span class="property-card-value">
+<div class="property-card-box card-box card-block estilo_carta_casa_texto">
+    <h3 class="titulo_carta_casa"><a class="titulo_carta_casa" href="<?php echo $generales->url_base; ?>hogares/hogar-beatriz.php">Hogar Beatriz</a></h3>
+    <div class="property-card-descr texto_carta_casa">
+        <p class="texto_carta_casa">Dirección Lázaro Pavia 3436,</p>
+        <p class="texto_carta_casa">Beatriz Hernández, 44760</p>
+        <span class="property-card-value" id="texto_carta_casa">
                 <i class="fa fa-dollar"><?php echo $costo ?></i>
             </span>
-        </div>
-    </div>
+    </div> 
+
 </div>
