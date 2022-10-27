25f68a26f0d5376cfef1bc8c88468a0ca5acff44
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-10-26 19:32:59 -0500

    v0.34.1 se generan botones de estados en donde se ubican hogares

---
+++ templates/mapa/menu_mapa.php
@@ -0,0 +1,35 @@
+
+                <!--<button class="boton_mapa">Guadalajara</button>
+                <button class="boton_mapa">Tonalá</button>
+                <button class="boton_mapa">Zapopan</button> !-->
+
+                <div class="btn-group">
+                    <button type="button" class="btn dropdown-toggle boton_mapa" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Guadalajara
+                    </button>
+                    <div class="dropdown-menu contenido_boton_mapa_text ">
+                        <p class="numero_boton_mapa">1</p>
+                        <p class="texto_boton_mapa">Propiedades</p>
+                        <p class="texto_boton_mapa estilo_redondo_abajo" >disponible</p>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn dropdown-toggle boton_mapa" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Tonalá
+                    </button>
+                    <div class="dropdown-menu contenido_boton_mapa_text ">
+                        <p class="numero_boton_mapa">3</p>
+                        <p class="texto_boton_mapa">Propiedades</p>
+                        <p class="texto_boton_mapa estilo_redondo_abajo" >disponible</p>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn dropdown-toggle boton_mapa" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Zapopan
+                    </button>
+                    <div class="dropdown-menu contenido_boton_mapa_text ">
+                        <p class="numero_boton_mapa">5</p>
+                        <p class="texto_boton_mapa">Propiedades</p>
+                        <p class="texto_boton_mapa estilo_redondo_abajo" >disponible</p>
+                    </div>
+                </div>
--- templates/mapa/texto_mapa.php
+++ templates/mapa/texto_mapa.php
@@ -1,12 +1,12 @@
-<div class="container-fluid h-100" style="background-color: #0000CC; padding: 0px;">
+<div class="container-fluid h-100 color_azul contenedor_mapa_text">
     <div class="row w-100 align-items-center">
         <div class="col text-center">
-            <div class="container">
-                <br>
+            <div class="container ">
+
                 <h1 class="texto_mapa"> Explora nuestras propiedades desde el mapa</h1>
                 <br>
             </div>
-
+            <?php include 'templates/mapa/menu_mapa.php'?>
             
         </div>
     </div>
