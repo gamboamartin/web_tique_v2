9c72a2a4fdcdb0a7313e08b51f7599e0aa427513
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-10-27 14:30:30 -0500

    v0.35.1 se ingresa mapa a la seccion de mapa en index

--- templates/mapa/texto_mapa.php
+++ templates/mapa/texto_mapa.php
@@ -1,3 +1,9 @@
+<?php
+use config\generales;
+include "./init.php";
+require './vendor/autoload.php';
+$generales = new generales();
+?>
 <div class="container-fluid h-100 color_azul contenedor_mapa_text">
     <div class="row w-100 align-items-center">
         <div class="col text-center">
@@ -6,8 +12,8 @@
                 <h1 class="texto_mapa"> Explora nuestras propiedades desde el mapa</h1>
                 <br>
             </div>
-            <?php include 'templates/mapa/menu_mapa.php'?>
-            
+            <?php include $generales->path_base.'templates/mapa/menu_mapa.php'?>
+            <?php include $generales->path_base.'templates/ubicaciones/_ubicaciones_hogares.php'?>
         </div>
     </div>
 </div>
