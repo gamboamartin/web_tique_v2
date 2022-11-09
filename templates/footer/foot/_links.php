3412bae1fe993aeadd81e97f4eabfdb286926ba0
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-11-08 20:14:46 -0600

    v0.58.2 se modifica foter bottom

--- templates/footer/foot/_links.php
+++ templates/footer/foot/_links.php
@@ -4,7 +4,7 @@ $generales = new generales();
 ?>
 <div class="col-md-3 col-sm-6" >
     <ul class="ul_links_footer borde_footer_contenido list">
-        <li class="padding-20 ho"><a class="texto_footer"  href="<?php echo $generales->url_base; ?>index.php" target="_blank">Home</a></li>
+        <li class="padding-20 "><a class="texto_footer"  href="<?php echo $generales->url_base; ?>index.php" target="_blank">Home</a></li>
         <li class="padding-20"><a class="texto_footer" href="<?php echo $generales->url_base; ?>elige-tu-hogar.php" target="_blank">Compra</a></li>
         <li class="padding-20 "><a class="texto_footer" href="<?php echo $generales->url_base; ?>compramos_casa.php" target="_blank">Vende</a></li>
     </ul >
