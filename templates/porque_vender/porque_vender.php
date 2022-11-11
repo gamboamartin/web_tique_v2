5610175adb2c936ec57d5c7cc6717efc6397f43f
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-11-11 16:48:16 -0600

    v0.64.2 se ingresan imagenes y texto a la vista porque vendernos tu casa

--- templates/porque_vender/porque_vender.php
+++ templates/porque_vender/porque_vender.php
@@ -1,10 +1,27 @@
+<?php
+use config\generales;
+$generales = new generales();
+?>
 
-            <div class="container contenedor_whats">
-                <br>
-                <h1 class="whatsapp_titulo">¿Por qué vendernos tu casa?</h1>
-                <br>
+<div class="">
+    <h1 class="whatsapp_titulo">¿Por qué vendernos tu casa?</h1>
+</div>
+<div class="row">
+    <div class="col-md-12 contenedor_porque_vender">
+        <div class="col-md-4" align="center">
 
-
-
-
+                <img  class="img_pq_vender" src="<?php echo $generales->url_base; ?>assets/img/img_vende/ICONO-17.png" height="100%"/>
+                <hi class="vender_titulo_negro">Pago de contado</hi>
+                <p class="texto_vender">Recibe tu dinero puntual y en el plazo acordado.</p>
         </div>
+        <div class="col-md-4 lineas_paralelas" align="center">
+            <img  class="img_pq_vender" src="<?php echo $generales->url_base; ?>assets/img/img_vende/ICONO-18.png" height="100%"/>
+            <hi class="vender_titulo_negro">¿Tiene adeudos?</hi>
+            <p class="texto_vender">No importa si tu casa tiene adeudo hipotecario o de serviciosnosotros liquidamos tus deudas.</p>
+        </div>
+        <div class="col-md-4" align="center">
+            <img  class="img_pq_vender" src="<?php echo $generales->url_base; ?>assets/img/img_vende/ICONO-19.png" height="100%"/>
+            <hi class="vender_titulo_negro">Garantía Tique</hi>
+            <p class="texto_vender">Realizamos todas las formalidades de un proceso de compra venta</p>
+        </div>
+</div>
