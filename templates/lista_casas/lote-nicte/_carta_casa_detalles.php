1b3c314a8a0eef1401ece63b170104a3fddf6b50
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2023-01-02 21:41:49 -0600

    v0.129.7 se agrega carta casa de lote nicte

--- templates/lista_casas/lote-nicte/_carta_casa_detalles.php
+++ templates/lista_casas/lote-nicte/_carta_casa_detalles.php
@@ -1,7 +1,7 @@
 <?php
 use config\generales;
 $generales = new generales();
-$numero=7134000.;
+$numero=3180000;
 $costo= number_format($numero, 2);
 $a = explode(",", $costo);
 if(count($a)>2) {
@@ -11,9 +11,9 @@ if(count($a)>2) {
 ?>
 
 <div class="property-card-box card-box card-block estilo_carta_casa_texto">
-    <h3 class="titulo_carta_casa"><a class="titulo_carta_casa" href="<?php echo $generales->url_base; ?>hogares/lote-itze.php">Lote-Itze</a></h3>
+    <h3 class="titulo_carta_casa"><a class="titulo_carta_casa" href="<?php echo $generales->url_base; ?>hogares/lote-nicte.php">Lote-Nicte</a></h3>
     <div class="property-card-descr texto_carta_casa">
-        <p class="texto_carta_casa">Paseo del Palomar 250, El Palomar, Tlajomulco de Zuñiga, Jalisco.</p>
+        <p class="texto_carta_casa"> P.º del Puma 3064, Ciudad Bugambilias, 45237 Zapopan, Jal.</p>
         <span class="property-card-value" id="texto_carta_casa">
                 <i class="fa fa-dollar"><?php echo $costo ?></i>
         </span>
