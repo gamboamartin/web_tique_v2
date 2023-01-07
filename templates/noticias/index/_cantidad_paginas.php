e525418a727ca597f616f10d5df0399ad859c1a8
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2023-01-06 19:42:17 -0600

    v0.141.8 se modifica indicador de slider en segmento de que dicen nuestros clientes (testimonios)

--- templates/noticias/index/_cantidad_paginas.php
+++ templates/noticias/index/_cantidad_paginas.php
@@ -1,5 +1,5 @@
 <ul class="carousel-indicators abajo_10" >
     <li id="viñeta_slider" data-target="#news-carousel" data-slide-to="0" class="active" ></li>
     <li id="viñeta_slider" data-target="#news-carousel" data-slide-to="1" ></li>
-    
+
 </ul>
