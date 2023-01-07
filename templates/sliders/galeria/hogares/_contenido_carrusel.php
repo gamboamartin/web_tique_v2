68d854846c224422081d15597eb7b66253b4e099
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2023-01-06 19:49:59 -0600

    v0.142.8 se modifica acomodo de hogares en slider y vista compra

--- templates/sliders/galeria/hogares/_contenido_carrusel.php
+++ templates/sliders/galeria/hogares/_contenido_carrusel.php
@@ -17,37 +17,47 @@ $generales = new generales();
         <?php include $generales->path_base.'templates/lista_casas/lote-yaax/_carta_casa.php' ?>
     </div>
     <div class="item ">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-xadani/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-pasitea/_carta_casa.php' ?>
+
     </div>
     <div class="item ">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-pasitea/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-xadani/_carta_casa.php' ?>
+
     </div>
     <div class="item ">
         <?php include $generales->path_base.'templates/lista_casas/hogar-orquidea/_carta_casa.php' ?>
+
     </div>
     <div class="item">
         <?php include $generales->path_base.'templates/lista_casas/hogar-xiadani/_carta_casa.php' ?>
+
     </div>
     <div class="item">
         <?php include $generales->path_base.'templates/lista_casas/hogar-eirene/_carta_casa.php' ?>
+
     </div>
     <div class="item">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-beatriz/_carta_casa.php' ?>
-    </div>
-    <div class="item">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-cihualpilli/_carta_casa.php' ?>
-    </div>
-    <div class="item">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-santiago/_carta_casa.php' ?>
-    </div>
-    <div class="item">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-aruna/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-venus/_carta_casa.php' ?>
+
     </div>
     <div class="item">
         <?php include $generales->path_base.'templates/lista_casas/hogar-eden/_carta_casa.php' ?>
+
+    </div>
+    <div class="item">
+        <?php include $generales->path_base.'templates/lista_casas/hogar-aruna/_carta_casa.php' ?>
+
+    </div>
+    <div class="item">
+        <?php include $generales->path_base.'templates/lista_casas/hogar-santiago/_carta_casa.php' ?>
+
+    </div>
+    <div class="item">
+        <?php include $generales->path_base.'templates/lista_casas/hogar-cihualpilli/_carta_casa.php' ?>
+
     </div>
     <div class="item ">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-venus/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-beatriz/_carta_casa.php' ?>
     </div>
 
 </div>
