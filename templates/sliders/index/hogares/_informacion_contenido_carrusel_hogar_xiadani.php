fdde2c7b82718434b3b9cea8abbcbf1ecfb60f84
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2023-04-03 19:44:19 -0600

    v0.225.14 se modifica status de apartado en hogar xiadani

--- templates/sliders/index/hogares/_informacion_contenido_carrusel_hogar_xiadani.php
+++ templates/sliders/index/hogares/_informacion_contenido_carrusel_hogar_xiadani.php
@@ -5,5 +5,8 @@ $generales = new generales();
 <div class="carousel-caption">
     <h3 class="carousel-caption-title"><span style="background: rgba(153, 153, 153, 0.5);" class=" nombre_hogar">Hogar Xiadani</span></h3>
     <!--<div class="s-description"><p>Ubicada en Zapopan</p></div>-->
+    <div class="contenedor_centrado">
+        <?php  include $generales->path_base.'templates/etiquetas_hogar/apartado.php' ?>
+    </div>
     <a href="<?php echo $generales->url_base;?>hogares/hogar-xiadani.php" class="btn btn-primary btn_info_casa"><span>Ver mi nuevo hogar</span></a>
 </div>
